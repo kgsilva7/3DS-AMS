59a42343c89823e2f4c8c90d15331209119f2a1d
Gustavo Alexandre da Silva <gustavoale1759@gmail.com>
2026-06-03 14:37:39 -0300

    Update settings.gradle.kts for plugin management

--- Programação em Aplicativos Mobile II/2º Bimestre/settings.gradle.kts
+++ Programação em Aplicativos Mobile II/2º Bimestre/settings.gradle.kts
@@ -1,6 +1,12 @@
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
@@ -12,5 +18,5 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "HardwareMaintenance"
+rootProject.name = "MyApplication"
 include(":app")
